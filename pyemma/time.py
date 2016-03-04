b0343d9d116fcd427fdd82a7e9fb3ddbb1e3ed61
nico <nicolas.deparis@astro.unistra.fr>
2016-03-04 14:19:42 +0100

    add somme files

---
+++ pyemma/time.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[3]:
+
+import numpy as np
+from scipy import integrate
+
+
+# In[6]:
+
+def a2t_quad(az, o_m, H0):
+    
+    o_v=1.-o_m
+    
+    H0 = H0*1e3/1e6/3.08567758e16*(365*24*3600) # Hubble constant in SI unit
+
+    def f(a):
+        return a/ np.sqrt(o_m*a + o_v*a**4 )
+
+    return 1./H0 * integrate.quad(f,0,az)[0]
+
