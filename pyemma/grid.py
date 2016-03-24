d0e44e596950dab85b831d7cfc16abec7886b435
nico <nicolas.deparis@astro.unistra.fr>
2016-03-24 11:04:56 +0100

    correct a ponderation in grid.py

--- pyemma/grid.py
+++ pyemma/grid.py
@@ -14,9 +14,9 @@ def get_cube(x,y,z,l,map,level,type, xmin=0.,xmax=1.,ymin=0.,ymax=1.,zmin=0.,zma
 
         n=2**dl
         if type=="2d":
-            return np.repeat(np.repeat(x,n,axis=0),n,axis=1)*n
+            return np.repeat(np.repeat(x,n,axis=0),n,axis=1)
         elif type=="3d":
-            return np.repeat(np.repeat(np.repeat(x,n,axis=0),n,axis=1),n,axis=2)*n
+            return np.repeat(np.repeat(np.repeat(x,n,axis=0),n,axis=1),n,axis=2)
         else:
             print("type should be 2d or 3d")
 
@@ -50,7 +50,7 @@ def get_cube(x,y,z,l,map,level,type, xmin=0.,xmax=1.,ymin=0.,ymax=1.,zmin=0.,zma
 
             if level==lmax:
                 mask=np.where(l>=level)[0]
-                w=map[mask] * np.power(0.5,3*(l[mask]-lmax)-1)
+                w=map[mask] * np.power(0.5,3*(l[mask]-lmax))
             else:
                 mask=np.where(l==level)[0]
                 w=map[mask]
